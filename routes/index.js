316e36ec13a8e9e79932988ce90e8d17578ce99d
李晓燕 <978387851@qq.com>
2017-05-07 17:52:02 +0800

    样式改为绿色   home界面替换

--- routes/index.js
+++ routes/index.js
@@ -13,5 +13,17 @@ router.get('/login.html', function(req, res, next) {
 router.get('/register.html', function(req, res, next) {
   res.render('home/register', { title: '注册' });
 });
+router.get('/introduction.html', function(req, res, next) {
+  res.render('home/introduction', { title: '商品信息' });
+});
+router.get('/pay.html', function(req, res, next) {
+  res.render('home/pay', { title: '订单支付' });
+});
+router.get('/search.html', function(req, res, next) {
+  res.render('home/search', { title: '搜索' });
+});
+router.get('/shopcart.html', function(req, res, next) {
+  res.render('home/shopcart', { title: '搜索' });
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('in', { title: '' });
+  res.render('information', { title: '个人中心' });
+});
+router.get('/information.html', function(req, res, next) {
+  res.render('person／information', { title: '个人中心' });
 });
 
 module.exports = router;
