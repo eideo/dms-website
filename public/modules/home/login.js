1d8d9fa460c42b881c0625d2fa9e2f2e6e55c319
Jeng <jeng.tam@udfex.com>
2017-05-09 17:43:12 +0800

    替换字体图标文件

--- public/modules/home/login.js
+++ public/modules/home/login.js
@@ -2,5 +2,22 @@
  * Created by tanxinzheng on 17/5/8.
  */
 $(document).ready(function() {
-
+    $("#login").click(function(){
+        $.ajax({
+            type: "POST",
+            data:{
+                password:$("#password").val(),
+                username:$("#username").val()
+            },
+            //dataType: "json",
+            contentType:"application/json",
+            url:"/api/member/login?password=111111&username=1500008843",
+            success: function(data){
+                console.log(data);
+            },
+            error: function(data){
+                console.log(data);
+            }
+        });
+    });
 });
